3484686b8b0dea62d0ae59cd39187512daf046a1
frcskoh <frcskoh@gmail.com>
2017-08-31 00:31:18 +0800

    2.0.0

--- linux_oper.py
+++ linux_oper.py
@@ -8,11 +8,12 @@ def ShellRun(command, hint = None, output = False):
     try:
         if output:
             output_text = os.popen(command)
+            if hint: trprint(hint)
+            return(output_text)
         else:
             os.system(command)
-        if hint:
-            trprint(hint)
-        return(output_text)
+            if hint: trprint(hint)
+            return(output_text)
     except:
         trprint('Error. Try to excute "%s" but failed. ', command)
 
@@ -22,10 +23,6 @@ def get_ip():
         return Get('http://checkip.dyndns.org/').text.split(':')[1].strip().split('<')[0]
     except:
         trprint('Network error. ')
-    
-def info_trprint(sp):
-    os.system('echo ' + sp.center(100, '*') + '\n')
-    return None
 
 def task_kill(name):
     k = os.popen('ps -aux').read().split('\n')[1:]
@@ -35,15 +32,16 @@ def task_kill(name):
             if i.split()[10] == name:
                 os.system('kill -9 ' + i.split()[1])
                 trprint("Found and kill the %s." & (i.split()[10]))
-        except:
-            pass
+        except OSError:
+            trprint("System Error.")
+            
     trprint("Task %s has been kill. " % (name))
     
 def reg_setup(cof, command):
     try:
         with open('/etc/crontab', 'a+') as f:
             f.write("%s %s \n" % (cof, command))
-    except:
+    except IOError:
         trprint("reg_setup Error.")
     else:
         trprint("Added %s successfully." % (command))
@@ -55,8 +53,8 @@ def apt_install(app):
     try:
         trprint('Installing ' + app.split()[0])
         ShellRun('apt-get install -y ' + app)
-    except:
-        trprint('Error !')
+    except OSError:
+        trprint('apt installation Error !')
     else:
         trprint("Installed %s successfully." % (app.split()[0]))
 
@@ -64,7 +62,7 @@ def port_unlocker(port):
     for j in [i.split()[1] for i in os.popen("".join(['lsof -i :', port])).read().split('\n')[1:-1]]:
         try:
             ShellRun("kill -9 %s" % (j))
-        except:
-            trprint('Error !')
+        except OSError:
+            trprint('port_unlocker Error !')
         else:
             trprint("Killed task : PID %s" % (j))
